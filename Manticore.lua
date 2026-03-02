5189d866e9ec42ebe20bbc0542ebf59cd252ffba
Impgh1011 <143576184+pgh1011@users.noreply.github.com>
2026-03-02 16:35:01 +0900

    Add Manticore.lua with script loading
    Added Manticore.lua script with initial setup and loading functionality.

---
+++ Manticore.lua
@@ -0,0 +1,3 @@
+-- 만티코어 정식 버전
+getgenv().SCRIPT_KEY = "2d0bbb12-76a1-4a84-8454-9eee145c31fd"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/cc7bf862fb2c68c053b658ec7c2b8e07df31b994e072a16c1a9f3f985521473b/download"))()
